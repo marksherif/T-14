4fb62ae740d2d1d826da4e512edeec1fe693f44a
Mark <marksherifjoseph@gmail.com>
2018-02-27 18:27:25 +0200

    Sprint 1 project fixed

---
+++ sprint_1_frontend/angular-seed-project/src/app/app-routing.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbRegisterComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
+
+export const routes: Routes = [
+    {
+      path: 'dashboard',
+      loadChildren: './dashboard/dashboard.module#DashboardModule'
+    },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '**', redirectTo: 'dashboard' }
+
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+
+];
+
+// const routes: Routes = [
+//   {
+//     path: 'dashboard',
+//     loadChildren: './dashboard/dashboard.module#DashboardModule'
+//   },
+//   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+//   { path: '**', redirectTo: 'dashboard' }
+// ];
+
+const config: ExtraOptions = {
+  useHash: true
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule {}
