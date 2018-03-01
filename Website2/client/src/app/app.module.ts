e3b4924435546feb63f5204d21fd804c41371727
MustafaSalama <mustafa.salama97@hotmail.com>
2018-03-01 17:11:08 +0200

    'all'

--- Website2/client/src/app/app.module.ts
+++ Website2/client/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { StoreC1Component } from './StoreC1/StoreC1.component';
+import { StoreC3Component } from './StoreC3/StoreC3.component';
+import { StoreC2Component } from './StoreC2/StoreC2.component';
 import { StoreC4Component } from './StoreC4/StoreC4.component';
 import { StoreC5Component } from './StoreC5/StoreC5.component';
 import { AuthenticationService } from './authentication.service';
@@ -19,6 +22,9 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
+  { path: 'StoreC1', component: StoreC1Component },
+  { path: 'StoreC2', component: StoreC2Component },
+  { path: 'StoreC3', component: StoreC3Component },
   { path: 'StoreC4', component: StoreC4Component },
   { path: 'StoreC5', component: StoreC5Component }
 
@@ -31,6 +37,9 @@ const routes: Routes = [
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    StoreC1Component,
+    StoreC2Component,
+    StoreC3Component,
     StoreC4Component,
     StoreC5Component
   ],
