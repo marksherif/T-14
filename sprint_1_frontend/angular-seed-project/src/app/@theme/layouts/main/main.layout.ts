553b561b78c25b64119d3f3af522b3ac437fa096
rafik-mikhail <rafik.mikhail@student.guc.du.eg>
2018-03-01 11:29:55 +0200

    Rafik update

--- sprint_1_frontend/angular-seed-project/src/app/@theme/layouts/main/main.layout.ts
+++ sprint_1_frontend/angular-seed-project/src/app/@theme/layouts/main/main.layout.ts
@@ -58,7 +58,9 @@ import 'rxjs/add/operator/delay';
           <p id="p">Youssef Shoeb</p>
     <a ><img id="y" src="../../../../assets/Shoeb.jpg" /></a>
       <p id="p">Youssef Omar Shoeb 37-0955</p>
-
+      <p id="p">Rafik</p>
+<a ><img id="y" src="../../../../assets/Rafik.PNG" /></a>
+  <p id="p">Rafik Sherif Louis 37-7419</p>
 
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
